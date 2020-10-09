1e57fe50626d6d95b1d8b017ef983c2eb7d8c9e3
andrewjohnroy <andrewjohnroy@gmail.com>
2020-10-10 03:21:25 +1100

    Create top.cpp

---
+++ top.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+#include <string>
+
+void main() {
+	std::string s = "top";
+	OpenClipboard(0);
+	EmptyClipboard();
+	HGLOBAL hg = GlobalAlloc(GMEM_MOVEABLE, s.size()+1);
+	if (!hg) {
+		CloseClipboard();
+		return;
+	}
+	memcpy(GlobalLock(hg), s.c_str(), s.size());
+	GlobalUnlock(hg);
+	SetClipboardData(CF_TEXT, hg);
+	CloseClipboard();
+	GlobalFree(hg);
+}
